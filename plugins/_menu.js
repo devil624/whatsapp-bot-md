f311081390f8262ea33761cbad9d2e15fb1d0e9c
ᴢᴇʏʀᴏx <97727454+devil624@users.noreply.github.com>
2022-08-25 11:46:04 +0400

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -12,7 +12,7 @@ const {
         jidToNum,
         genThumbnail,
 } = require('../lib/')
-const { VERSION, FOOTERMARK, BOT_INFO, } = require('../config')
+const { VERSION, FOOTERMARK, BOT_INFO, MENU_MEDIA, } = require('../config')
 bot.addCommand(
 	{
 		pattern: 'help ?(.*)',
@@ -267,4 +267,48 @@ bot.addCommand(
     }
 );
 
+bot.addCommand(
+	{
+		pattern: 'vmenu ?(.*)',
+		fromMe: true,
+		dontAddCommandList: true,
+	},
+	async (message, match) => {
+        const date = new Date()
+		await message.send(
+            await genHydratedButtons( [{ button: { id: 'ping', text: '𝐒𝐏𝐄𝐄𝐃 𝐓𝐄𝐒𝐓' } },{ button: { id: 'cmd', text: '𝐀𝐋𝐋 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒' } }, { button: { id: 'gruplinkgib', text: '𝐒𝐔𝐏𝐏𝐎𝐑𝐓' } },],
+                `\n╔════⟬ ${BOT_INFO.split(",")[1]} ⟭════❃
+║
+╠❐  𝐎𝐖𝐍𝐄𝐑 : ${BOT_INFO.split(",")[0]}
+║
+╠❐  𝐏𝐑𝐄𝐅𝐈𝐗 : ${PREFIX}
+║
+╠❐  𝐔𝐒𝐄𝐑 : ${message.pushName}
+║
+╠❐  𝐓𝐈𝐌𝐄 : ${date.toLocaleTimeString()}
+║
+╠❐  𝐃𝐀𝐘 : ${date.toLocaleString('en', { weekday: 'long' })}
+║
+╠❐  𝐃𝐀𝐓𝐄 : ${date.toLocaleDateString('hi')}
+║
+╠❐  𝐕𝐄𝐑𝐒𝐈𝐎𝐍 : ${VERSION}
+║
+╠❐  𝐓𝐎𝐓𝐀𝐋 𝐏𝐋𝐔𝐆𝐈𝐍𝐒 : ${PLUGINS.count}
+║
+╠❐  𝐔𝐏𝐓𝐈𝐌𝐄 : ${getUptime('t')}
+║ 
+║
+║       
+║   ▎▍▌▌▎▌▉▐▏▌▎▍▌▌▌
+║   ▎▍▌▌▎▌▉▐▏▌▎▍▌▌▌
+║   
+║                ${BOT_INFO.split(",")[1]}
+╚═══════════════════❃\n`,
+		    `${FOOTERMARK}`, message ,{video: `${MENU_MEDIA.split(",")[1]}`}
+            ),
+            {},
+            'template'
+        ) 
+ }
+)
 
